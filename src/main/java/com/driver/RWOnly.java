365fca79814dcf117903049b8efe81961e20b044
CodingSwami <113988978+CodingSwami@users.noreply.github.com>
2023-05-11 13:25:59 +0530

    Working RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,14 @@ package com.driver;
 
 public class RWOnly {
     private String name;
+    
+    public String get()
+    {
+        return name;
+    }
+    
+    public void set(String str)
+    {
+        this.name= str;
+    }
 }
