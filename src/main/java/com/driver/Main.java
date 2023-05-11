5e760dd88b4028c6d7f403d3ada94e24fd584298
CodingSwami <113988978+CodingSwami@users.noreply.github.com>
2023-05-11 13:24:48 +0530

    Working Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[])
+  {
+    RWOnly obj= new RWOnly();
+    
+   /* obj.name= "Gaurav";
+    error: name has private access in RWOnly
+    obj.name= "Gaurav";
+    */
+    
+    obj.set("Gaurav");
+    System.out.println(obj.get());
+  }
 }
